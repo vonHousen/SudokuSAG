682322bff0d2ea149d1cb958aa01f94e7eecbac6
vonHousen <42911623+vonHousen@users.noreply.github.com>
2020-01-18 18:55:54 +0100

    Developed Table's negotiations tests.

--- src/test/java/sudoku/TableTest.java
+++ src/test/java/sudoku/TableTest.java
@@ -75,8 +75,12 @@ public class TableTest
 		// Check Table's response for Player's offers
 		theTable.tell(new Table.OfferMsg(1, 1, playerDummy_1.getRef(), 0));
 		playerDummy_1.expectNoMessage();
-		theTable.tell(new Table.OfferMsg(2, 2, playerDummy_2.getRef(), 9));
 		playerDummy_2.expectNoMessage();
+		playerDummy_3.expectNoMessage();
+		theTable.tell(new Table.OfferMsg(2, 2, playerDummy_2.getRef(), 9));
+		playerDummy_1.expectNoMessage();
+		playerDummy_2.expectNoMessage();
+		playerDummy_3.expectNoMessage();
 		theTable.tell(new Table.OfferMsg(3, 3, playerDummy_3.getRef(), 18));
 		Player.AdditionalInfoRequestMsg response1 = (Player.AdditionalInfoRequestMsg) playerDummy_1.receiveMessage();
 		Player.AdditionalInfoRequestMsg response2 = (Player.AdditionalInfoRequestMsg) playerDummy_2.receiveMessage();
@@ -84,5 +88,46 @@ public class TableTest
 		assertTrue(Arrays.equals(response1._otherDigits, new int[]{2, 3}));
 		assertTrue(Arrays.equals(response2._otherDigits, new int[]{1, 3}));
 		assertTrue(Arrays.equals(response3._otherDigits, new int[]{1, 2}));
+
+		// Send to the Table more info about other offers
+		theTable.tell(new Table.AdditionalInfoMsg(
+				new int[]{2, 3}, new float[]{5L, 3L}, new boolean[]{false, false}, playerDummy_1.getRef(), 0));
+		playerDummy_1.expectNoMessage();
+		playerDummy_2.expectNoMessage();
+		playerDummy_3.expectNoMessage();
+		theTable.tell(new Table.AdditionalInfoMsg(
+				new int[]{1, 3}, new float[]{100L, 2L}, new boolean[]{true, false}, playerDummy_2.getRef(), 9));
+		Player.RejectOfferMsg responseReject = (Player.RejectOfferMsg) playerDummy_1.receiveMessage();
+		playerDummy_2.expectNoMessage();
+		playerDummy_3.expectNoMessage();
+		theTable.tell(new Table.AdditionalInfoMsg(
+				new int[]{1, 2}, new float[]{2L, 2L}, new boolean[]{false, false}, playerDummy_3.getRef(), 18));
+		playerDummy_1.expectNoMessage();
+		playerDummy_2.expectNoMessage();
+		playerDummy_3.expectNoMessage();
+
+		// Simulate replacing conflicting digit
+		theTable.tell(new Table.WithdrawOfferMsg(1, playerDummy_1.getRef(), 0));
+		theTable.tell(new Table.OfferMsg(8, 3, playerDummy_1.getRef(), 0));
+		playerDummy_1.expectNoMessage();
+		Player.AdditionalInfoRequestMsg response4 = (Player.AdditionalInfoRequestMsg) playerDummy_2.receiveMessage();
+		Player.AdditionalInfoRequestMsg response5 = (Player.AdditionalInfoRequestMsg) playerDummy_3.receiveMessage();
+		assertTrue(Arrays.equals(response4._otherDigits, new int[]{8}));
+		assertTrue(Arrays.equals(response5._otherDigits, new int[]{8}));
+
+		// Send to the Table more info about replaced offer
+		theTable.tell(new Table.AdditionalInfoMsg(
+				new int[]{8}, new float[]{1L}, new boolean[]{false, false}, playerDummy_2.getRef(), 9));
+		playerDummy_1.expectNoMessage();
+		playerDummy_2.expectNoMessage();
+		playerDummy_3.expectNoMessage();
+		theTable.tell(new Table.AdditionalInfoMsg(
+				new int[]{8}, new float[]{2L}, new boolean[]{false, false}, playerDummy_3.getRef(), 18));
+		Player.NegotiationsPositiveMsg responseOK1 = (Player.NegotiationsPositiveMsg) playerDummy_1.receiveMessage();
+		Player.NegotiationsPositiveMsg responseOK2 = (Player.NegotiationsPositiveMsg) playerDummy_2.receiveMessage();
+		Player.NegotiationsPositiveMsg responseOK3 = (Player.NegotiationsPositiveMsg) playerDummy_3.receiveMessage();
+		assertEquals(2, responseOK1._approvedDigit);
+		assertEquals(2, responseOK2._approvedDigit);
+		assertEquals(2, responseOK3._approvedDigit);
 	}
 }

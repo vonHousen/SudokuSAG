d550e2292a700f2599b75201745bf22db1fd45ff
Emil Bałdyga <emil.dzid@gmail.com>
2020-01-28 13:23:15 +0100

    Added new 9x9 easy sudoku.

--- src/test/java/sudoku/SudokuStartTest.java
+++ src/test/java/sudoku/SudokuStartTest.java
@@ -518,3 +518,63 @@ public class SudokuStartTest
 		assertEquals(sudokuSolution, sudokuResults);
 	}
 }
+
+	@Test
+	public void test_12_SolvingSudoku()
+	{
+		int NO = 12;
+		int chances = 100;
+		int rank = 3;
+		int[][] naturalBoard = {
+				{0,8,0,0,0,0,2,0,0},
+				{0,0,0,0,8,4,0,9,0},
+				{0,0,6,3,2,0,0,1,0},
+				{0,9,7,0,0,0,0,8,0},
+				{8,0,0,9,0,3,0,0,2},
+				{0,1,0,0,0,0,9,5,0},
+				{0,7,0,0,4,5,8,0,0},
+				{0,3,0,7,1,0,0,0,0},
+				{0,0,8,0,0,0,0,4,0}
+		};
+		int[][] naturalSolution = {
+				{7,8,4,1,9,6,2,3,5},
+				{3,2,1,5,8,4,6,9,7},
+				{9,5,6,3,2,7,4,1,8},
+				{2,9,7,4,5,1,3,8,6},
+				{8,4,5,9,6,3,1,7,2},
+				{6,1,3,8,7,2,9,5,4},
+				{1,7,9,6,4,5,8,2,3},
+				{4,3,2,7,1,8,5,6,9},
+				{5,6,8,2,3,9,7,4,1}
+		};
+		Sudoku sudoku = createSudokuFromNaturalBoard(rank, naturalBoard);
+		Sudoku sudokuSolution = createSudokuFromNaturalBoard(rank, naturalSolution);
+		Sudoku prevSudoku;
+
+		// create the Supervisor & Teacher
+		TestProbe<SudokuSupervisor.Protocol> dummyGuardian = testKit.createTestProbe();
+		ActorRef<Teacher.Protocol> theTeacher = testKit.spawn(Teacher.create(
+				new Teacher.CreateMsg("teacher-" + NO, sudoku, dummyGuardian.getRef())
+		), "test-" + NO);
+
+		SudokuSupervisor.IterationFinishedMsg results =
+				(SudokuSupervisor.IterationFinishedMsg) dummyGuardian.receiveMessage(Duration.ofSeconds(10000));
+		Sudoku sudokuResults = results._newSolution;
+		// see how good are first iteration's results
+		//sudoku.printNatural();
+		//System.out.println();
+		//sudokuResults.printNatural();
+
+		while(!sudokuResults.equals(sudokuSolution) && chances-- > 0)		// give another chance
+		{
+			results = (SudokuSupervisor.IterationFinishedMsg) dummyGuardian.receiveMessage(Duration.ofSeconds(15));
+			prevSudoku = sudokuResults;
+			sudokuResults = results._newSolution;
+			if(prevSudoku.equals(sudokuResults))
+				break;
+			//System.out.println();
+			//sudokuResults.printNatural();
+		}
+		sudokuResults.printNatural();
+		assertEquals(sudokuSolution, sudokuResults);
+	}
